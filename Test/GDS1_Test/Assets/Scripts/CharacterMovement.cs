39902ac9fd71f57789a477cd774f9afa77b54c07
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-04-16 16:00:17 +1000

    S2US2, 8 + 18 implemented
    S2US2: As a player I do not want the camera to rotate when I rotate the player so that I can move the character with ease.
    S2US8: As a player I want to be able to control the camera movement with the mouse so that I can play with ease.
    S2US18: As a player I would prefer for the camera to be higher and further away so that I can see more of the map and do not have to rely on keys to move the camera.

--- Test/GDS1_Test/Assets/Scripts/CharacterMovement.cs
+++ Test/GDS1_Test/Assets/Scripts/CharacterMovement.cs
@@ -45,9 +45,9 @@ public class CharacterMovement : MonoBehaviour
     
 
 
-        float rot = Input.GetAxis("Horizontal") * rotationSpeed;
-        rot *= Time.deltaTime;
-        transform.Rotate(0, rot, 0);
+    //    float rot = Input.GetAxis("Horizontal") * rotationSpeed;
+   //     rot *= Time.deltaTime;
+ //       transform.Rotate(0, rot, 0);
         
 
             if (trans != 0)
@@ -102,5 +102,15 @@ public class CharacterMovement : MonoBehaviour
               //  }
             }
         }
+
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
