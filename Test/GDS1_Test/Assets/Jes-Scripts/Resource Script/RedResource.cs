9f9c01c6c36d26c99da9008604e99bf9c932bd40
Jesica Nikolovska <jesica.nikolovska@student.uts.edu.au>
2023-04-26 18:39:00 +1000

    final bug fix

--- Test/GDS1_Test/Assets/Jes-Scripts/Resource Script/RedResource.cs
+++ Test/GDS1_Test/Assets/Jes-Scripts/Resource Script/RedResource.cs
@@ -6,7 +6,7 @@ public class RedResource : MonoBehaviour
 {
    public int pickup;
   public  ResourcePickUP resourcePickUP;
-    public GameObject gem;
+   // public GameObject gem;
     public ItemClass redResource;
 
     private void Start()
--- Test/GDS1_Test/Assets/script.cs
+++ Test/GDS1_Test/Assets/script.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+  
     // Start is called before the first frame update
     void Start()
     {
