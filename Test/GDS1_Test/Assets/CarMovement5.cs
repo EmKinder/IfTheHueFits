0d1a39f6c5763c5e6c4c04fbcefa8d0275ef4ba1
Jesica Nikolovska <jesica.nikolovska@student.uts.edu.au>
2023-05-10 18:26:06 +1000

    S3US67:- Done
    S3US67:As a player I want obstacles such as cars to be introduced within the levels so that I am challenged estimation (1hour)- Jesica

---
+++ Test/GDS1_Test/Assets/CarMovement5.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement5 : MonoBehaviour
+{
+    public Rigidbody car;
+    public float carSpeed;
+    public MeshRenderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rend.isVisible)
+        {
+            car.velocity = gameObject.transform.forward * carSpeed;
+            Debug.Log("carVisible");
+        }
+    }
+}
